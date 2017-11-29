8f5f76969d36a35780d4857dbe0e7d503043c3e9
Paul <pmessina@invap.com.ar>
2017-11-29 09:02:30 -0300

    Agregado de timezone-arg.sh

---
+++ tests/timezone-arg.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+#Desc: La timezone debe estar en ARS o en -03
+#===============================================================================
+#
+#        AUTHOR:  Paul
+#       COMPANY:  Dissociated Press
+#       VERSION:  1.0
+#      REVISION:  ---
+#===============================================================================
+
+
+programname=$0
+
+function usage {
+    echo "usage: $programname [-s] [-e]"
+    echo "  -s      show the test"
+    echo "  -e      execute the test"
+    exit 1
+}
+if [ $# -eq 1 ]
+then 
+    if [ $1 = "-s" ]
+    then 
+        DOFlag="show"
+    else
+        DOFlag="do"
+    fi
+else
+    usage
+fi
+
+if [ $DOFlag = "do" ]
+then 
+    out=$( date "+%Z" )
+    if [ $out = "-03" -o $out = "ARS" ]
+    then
+       echo "Ok"
+    else
+        echo "Not Ok"
+    fi
+fi
+
+if [ $DOFlag = "show" ]
+then
+    echo "La timezone debe estar en ARS o en -03"
+fi
+
